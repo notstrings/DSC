08a7742f79fbf7f3e4ce3c91fd92f269873baafb
notstrings@gmail.com <Honntouha1@>
2025-02-11 00:12:13 +0900

    first commit

---
+++ AutoInstall.ps1
@@ -0,0 +1,38 @@
+﻿# 管理者権限セッションでの実行を要求する
+function isInAdmin {
+    $UID = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    return $UID.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+if (-not (isInAdmin)) {
+    Start-Process powershell.exe -ArgumentList "-NoExit -File ""$($MyInvocation.MyCommand.Path)""" -Verb RunAs
+    exit 0
+}
+
+## WinGet #####################################################################
+
+# PSGalleryを信頼する
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+# DSC Modulesをインストール
+# Find-Module -Name NetworkingDSC  -Repository PSGallery | Install-Module
+# Find-Module -Name 7ZipArchiveDsc -Repository PSGallery | Install-Module
+# Find-Module -Name FileContentDsc -Repository PSGallery | Install-Module
+
+# DSC実行
+Write-Output 'Y' | winget configure --file "$($PSScriptRoot)\Environment.dsc.yaml"
+Write-Output "Y" | winget configure --file "$($PSScriptRoot)\Packages.dsc.yaml"
+
+# RoboCopy
+# robocopy "Source" "Destination" /MIR /FFT /DCOPY:DAT /R:3 /W:5 /NFL /NP /XJ 
+# robocopy "Source" "Destination" /MIR /FFT /DCOPY:DAT /R:3 /W:5 /NFL /NP /XJ 
+
+## Scoop ######################################################################
+
+if ((Get-Command scoop -ErrorAction SilentlyContinue) -eq $false) {
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+}
+scoop bucket add extras
+scoop install 7zip
+scoop install imagemagick
+scoop install ghostscript
+scoop install qpdf
